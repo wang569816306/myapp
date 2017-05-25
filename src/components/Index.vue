978d94ba8484ffc15041176761a8b88e4cf19c2a
wang569816306 <569816303@qq.com>
2017-05-25 18:07:59 +0800

    vueshop

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index">
+    我是index页面
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.index {
+  text-align: center;
+}
+
+</style>
